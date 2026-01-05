fbe31864c18aea14ba5c627e2eb7188859ad6438
AnmolTiwari71 <anmoltiwari7172@gmail.com>
2026-01-05 09:34:33 +0530

    Fix mailer SMTP config

--- mailer/index.js
+++ mailer/index.js
@@ -1,12 +1,26 @@
-const nodemailer = require("nodemailer")
+// const nodemailer = require("nodemailer")
+
+// const mailer = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: process.env.MAIL_SENDER,
+//         pass: process.env.MAIL_PASSWORD
+//     },
+// })
+// module.exports = mailer
+
+const nodemailer = require("nodemailer");
 
 const mailer = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.MAIL_SENDER,
-        pass: process.env.MAIL_PASSWORD
-    },
-})
-module.exports = mailer
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // MUST be true for 465
+  auth: {
+    user: process.env.MAIL_SENDER,
+    pass: process.env.MAIL_PASSWORD, // App Password only
+  },
+});
+
+module.exports = mailer;
